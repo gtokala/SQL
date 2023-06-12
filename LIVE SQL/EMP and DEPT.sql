6713a713af60b94f5e99c0bdc8580610936c437f
gtokala <gova.ece@gmail.com>
2023-06-12 11:55:50 -0400

    Update EMP and DEPT.sql

--- LIVE SQL/EMP and DEPT.sql
+++ LIVE SQL/EMP and DEPT.sql
@@ -1,1 +1,7 @@
-
+-- Create DEPT table which will be the parent table of EMP table.
+create table dept(
+  deptno number(2,0),
+  dname  varchar2(14),
+  loc    varchar2(13)
+  constraint pk_dept primary key (deptno)
+  )
