c3126f1064cdcf1eea38ef2d8c24eb0ae4e9924d
gtokala <gova.ece@gmail.com>
2023-06-12 16:29:14 -0400

    Update Practice.sql

--- Udemy Advanced SQL/Practice.sql
+++ Udemy Advanced SQL/Practice.sql
@@ -1,3 +1,4 @@
+--Topics TRIM, LTRIM, RTRIM, SUBSTR, REPLACE, REGEX_REPLACE
 select * from emp;
 select empno, lpad(empno,8,0), nvl(comm,0) from emp;
 select trim(' gova  '), length(trim(' gova  ')) from dual;
@@ -48,6 +49,105 @@ from dual;
 -- following example omits ' ' space 
 select REGEXP_REPLACE('some text, 123 someone, another text some1','[^a-zA-Z ]','')
 from dual;
+
+-- trim both
+SELECT trim(both '%' from '%%ABCDE_') FROM dual;
+SELECT trim(both '%' from '%%ABCDE_%') FROM dual;
+
+--replace()
+select replace('123abc123xyz123', '123') from dual;
+
+--trim
+select trim('123' from '123abc123xyz') from dual;
+
+-trim is case sensitive trim, ltrim and rtrim only works with single char
+SELECT TRIM('a' from 'AAAAnaaa'),
+       TRIM('A' from 'AAAAnaaa') 
+FROM DUAL;
+--out put is AAAAn, naaa
+
+-- both same output chars other than alphabets and numbers are replaced with nothing
+SELECT  LOWER(REGEXP_REPLACE(ASCIISTR('2019/023'), '[^a-zA-Z0-9]'))
+FROM   dual;
+SELECT  LOWER(REGEXP_REPLACE('2019/023', '[^a-zA-Z0-9]'))
+FROM   dual;
+SELECT  REGEXP_REPLACE('A2019/023', '[^a-zA-Z0-9]')
+FROM   dual;
+
+SELECT
+LTRIM('anaconda', 'an'),
+LTRIM('anaconda', 'na')
+FROM dual;
+
+with t as (
+select '%/?^ABC/123*#@' as s from dual union all
+select 'ABC/123*#@' as s from dual union all
+select '%/?^ABC/123' as s from dual union all
+select '%/?^' as s from dual union all
+select 'ABC/123' as s from dual
+)
+select s
+,regexp_replace(s,'^[^[:alnum:]]*(.*?)[^[:alnum:]]*$','\1') as s_10g
+,substr(s
+        , instr(s, substr(translate(s, 'X%/?^*#@', 'X'), 1, 1))
+        , instr(s, substr(translate(s,'X%/?^*#@','X'), -1, 1), -1)
+          - instr(s, substr(translate(s,'X%/?^*#@','X'), 1, 1))
+          + 1
+       ) as s_9i
+from t;
+
+/*
+S	              S_10G	  S_9I
+%/?^ABC/123*#@	ABC/123	ABC/123
+ABC/123*#@	    ABC/123	ABC/123
+%/?^ABC/123	    ABC/123	ABC/123
+%/?^	          -	      -
+ABC/123	        ABC/123	ABC/123
+*/
+
+select ltrim(rtrim(col1, '/'), '/') from (
+SELECT '/abcd/efght/' as col1 FROM dual
+)
+select trim(both '/' from '/abcd/efgh/') from dual;
+
+/*
+Pattern parts:
+
+(                -- start of Group #1
+  [^[:alnum:]]   -- any non-alphanumeric character 
+  |              -- or 
+  ^              -- start of string or start of line 
+)                -- end of Group #1
+(                -- start of Group #2
+  (              -- start of Group #3 
+    \d           -- any digit
+  )              -- end of Group #3
+  *              -- include in previous group zero or more consecutive digits
+  some           -- core string to match
+  (              -- start of group #4
+    \d           -- any digit
+  )              -- end of group #4  
+  *              -- include in previous group zero or more consecutive digits
+)                -- end of Group #2
+(                -- start of Group #5
+  [^[:alnum:]]   -- any non-alphanumeric character 
+  |              -- or
+  $              -- end of string or end of line
+)   
+*/
+select regexp_replace(
+  source_string,                                       -- source string
+  '([^[:alnum:]]|^)((\d)*some(\d)*)([^[:alnum:]]|$)',  -- pattern
+  '\1\5',                                              -- leave separators in place
+  1,                                                   -- start from beginning
+  0,                                                   -- replace all occurences
+  'im'                                                 -- case-insensitive and multiline 
+)
+from (
+SELECT 'some text, 123 someone, another text some1' as source_string FROM dual
+)
+
+
 
 
 
