35162743ad4cfce020574d6e0616f2cdfe8882c1
gtokala <gova.ece@gmail.com>
2023-06-12 23:12:26 -0400

    Update Pending Challenges.sql

--- Pending Challenges.sql
+++ Pending Challenges.sql
@@ -1,1 +1,7 @@
+-- trim reddy from first and last keep middle one
+select 'reddy govardhan reddy tokala reddy' from dual;
 
+--This query replaces all reddy words in string but the requirement is to keep the middle reddy
+--TRIM with more than one Char is working in MS SQL Server but not in Oracle. Oracle is only taking single char
+select replace(name, 'reddy') from
+(select 'reddy govardhan reddy tokala reddy' as name from dual);
