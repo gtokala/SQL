a3cac182ec51b983110a111a95efb2dbdddf8b2c
gtokala <gova.ece@gmail.com>
2023-06-13 00:02:45 -0400

    Update Pending Challenges.sql

--- Pending Challenges.sql
+++ Pending Challenges.sql
@@ -1,10 +1,21 @@
 -- trim reddy from first and last keep middle one
 select 'reddy govardhan reddy tokala reddy' from dual;
 
---This query replaces all reddy words in string but the requirement is to keep the middle reddy
+--This query replaces all reddy words in string but the requirement is to keep the middle reddy 
+--and output govardhan reddy tokala
 --TRIM with more than one Char is working in MS SQL Server but not in Oracle. Oracle is only taking single char
 select replace(name, 'reddy') from
 (select 'reddy govardhan reddy tokala reddy' as name from dual);
+/*Output
+govardhan tokala
+*/
+
+--Complete replacement string needs to match
+select replace(name, 'reddy') from
+(select '1eddy govardhan reddy tokala reddy' as name from dual);
+/*Output
+1eddy govardhan tokala
+*/
 
 --MS SQL SERVER
 select trim('reddy' from 'reddy govardhan reddy tokala reddy')
@@ -28,4 +39,22 @@ which states LEN "returns the number of characters of the specified string expre
 You need to instead use the DATALENGTH function - see 
 http://msdn.microsoft.com/en-us/library/ms173486(SQL.90).aspx
 - which "returns the number of bytes used to represent any expression".
+*/
+-- trim and suplied string
+select trim('reddy' from '1redd govardhan reddy tokala 1eddy')
+,trim('reddy' from 'redd1 govardhan reddy tokala 1eddy')
+,trim('reddy' from 'redd govardhan reddy tokala eddy')
+,trim('reddy' from 'reddy govardhan reddy tokala reddy')
+,trim('reddy' from 'reddyy govardhan reddy tokala reddyy')
+,trim('reddy' from 'rreddy govardhan reddy tokala rreddy')
+,trim('redy' from 'rreddry govardhan reddy tokala rreddyy')
+
+/*output
+1redd govardhan reddy tokala 1	
+1 govardhan reddy tokala 1	
+govardhan reddy tokala	
+govardhan reddy tokala	
+govardhan reddy tokala	
+govardhan reddy tokala	
+govardhan reddy tokala
 */
